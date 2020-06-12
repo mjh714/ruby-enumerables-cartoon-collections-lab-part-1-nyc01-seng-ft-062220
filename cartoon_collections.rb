a88a22d22fdb3fa6ba1af2ec60d1522ee03d4ab8
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-12 17:22:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
- array.each_with_idex do |character|
+ array.each_with_index do |character|
    puts index + 1 + ". " + character
  end
 end
