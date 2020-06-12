8db664c7a91d9c546dd2b1732f7f9fff9304ce9f
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-12 17:35:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def list_dwarves(array)
   str = ""
  array.each_with_index do |index, character|
-   str += " #{index + 1}. #{character}"
+   str += " #{index +1}. #{character}"
  end
  puts str
 end
